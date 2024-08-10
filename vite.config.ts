949033338918f11c375e210a2ff090bc9535c6a9
Lelik Jan <21alex7777@gmail.com>
2024-08-10 15:39:34 +0300

    git commit -m "chore(build): configure Vite for React component library
    - Set up Vite configuration to build the library with external dependencies for React and ReactDOM. Enabled source maps and targeted ESNext for modern features."
    - Remove src/App.css,src/App.tsx,src/index.css,src/main.ts
    - add src/index.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,37 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import { dependencies, devDependencies } from './package.json'
 
 export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.js'),
-      name: 'MyLib',
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: '@alex77/react-ui-kit',
+      formats: ['es'],
       // the proper extensions will be added
-      fileName: 'my-lib',
+      fileName: 'index',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: [
+        ...Object.keys(dependencies),
+        ...Object.keys(devDependencies),
+        'react/jsx-runtime',
+      ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          react:'React',
+          'react-dom':'ReactDOM',
         },
       },
     },
+    sourcemap: true,
+    //чтобы использовать последние фичи
+    target: 'esnext',
   },
 })
